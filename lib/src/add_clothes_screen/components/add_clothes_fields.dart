d3ed16e32568458e36239a9508229da0de5e3f7a
ke-saad <kellalisaad@gmail.com>
2024-11-01 12:16:34 +0100

    eleventh commit

--- lib/src/add_clothes_screen/components/add_clothes_fields.dart
+++ lib/src/add_clothes_screen/components/add_clothes_fields.dart
@@ -6,6 +6,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:image/image.dart' as img;
+import 'package:logger/logger.dart'; // Import the logger package
 
 class AddClothesFields extends StatefulWidget {
   final String userId;
@@ -18,6 +19,7 @@ class AddClothesFields extends StatefulWidget {
 
 class AddClothesFieldsState extends State<AddClothesFields> {
   final _formKey = GlobalKey<FormState>();
+  final Logger _logger = Logger(); // Initialize the logger
 
   String? _title;
   XFile? _image;
@@ -30,6 +32,8 @@ class AddClothesFieldsState extends State<AddClothesFields> {
   final FirebaseStorage _storage = FirebaseStorage.instance;
   Interpreter? _interpreter;
   List<String>? _labels;
+  bool _isModelLoaded = false; // Flag to track model loading
+  bool _isLoadingModel = true; // Flag for loading model process
 
   @override
   void initState() {
@@ -39,16 +43,54 @@ class AddClothesFieldsState extends State<AddClothesFields> {
 
   Future<void> _loadModel() async {
     try {
-      _interpreter = await Interpreter.fromAsset('model_unquant.tflite');
-      final labelsData =
-          await DefaultAssetBundle.of(context).loadString('assets/labels.txt');
-      _labels = labelsData.split('\n');
+      // Check if the model file exists in assets
+      if (await _checkAssetFileExists('assets/my_model.tflite')) {
+        _interpreter = await Interpreter.fromAsset('assets/my_model.tflite');
+        _isModelLoaded = true;
+        _logger.i("Model loaded successfully.");
+      } else {
+        _logger.e("Model file not found in assets.");
+      }
+
+      // Check if the labels file exists in assets
+      if (await _checkAssetFileExists('assets/labels.txt')) {
+        final labelsData =
+            await DefaultAssetBundle.of(context).loadString('assets/labels.txt');
+        _labels = labelsData.split('\n');
+        _logger.i("Labels loaded successfully.");
+      } else {
+        _logger.e("Labels file not found in assets.");
+      }
     } catch (e) {
-      print("Error loading model: $e");
+      _logger.e("Error loading model: $e");
+    } finally {
+      setState(() {
+        _isLoadingModel = false;
+      });
+    }
+  }
+
+  Future<bool> _checkAssetFileExists(String assetPath) async {
+    try {
+      final data = await DefaultAssetBundle.of(context).load(assetPath);
+      return data != null;
+    } catch (e) {
+      return false; // If loading fails, the asset doesn't exist
     }
   }
 
   Future<void> _pickImage() async {
+    // Check if the model is loaded before picking the image
+    if (_isLoadingModel) {
+      _logger.e("Model is still loading. Please wait.");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Model is still loading. Please wait."),
+        ),
+      );
+      return; // Exit the method if the model is still loading
+    }
+
     final picker = ImagePicker();
     final pickedImage = await picker.pickImage(source: ImageSource.gallery);
     if (pickedImage != null) {
@@ -60,54 +102,72 @@ class AddClothesFieldsState extends State<AddClothesFields> {
   }
 
   Future<void> _classifyImage(File image) async {
+    if (_interpreter == null || _labels == null) {
+      _logger.e(
+          "Image classification failed: model is not loaded or labels are null.");
+      return;
+    }
+
     final img.Image? imageInput = img.decodeImage(image.readAsBytesSync());
-    if (imageInput != null && _interpreter != null && _labels != null) {
+    if (imageInput != null) {
       final imageAsTensor = _preprocessImage(imageInput);
-      final output =
-          List.filled(1 * _labels!.length, 0).reshape([1, _labels!.length]);
+      final output = List.filled(1 * 3, 0).reshape([1, 3]); // Allocate for 3 classes
 
       _interpreter!.run(imageAsTensor, output);
 
       final confidenceScores = output[0];
-      final maxScoreIndex = confidenceScores.indexWhere(
-          (score) => score == confidenceScores.reduce((a, b) => a > b ? a : b));
+
+      // Find the index of the highest confidence score
+      int maxScoreIndex = 0;
+      double maxScore = confidenceScores[0];
+      for (int i = 1; i < confidenceScores.length; i++) {
+        if (confidenceScores[i] > maxScore) {
+          maxScore = confidenceScores[i];
+          maxScoreIndex = i;
+        }
+      }
 
       setState(() {
         _category = _labels![maxScoreIndex];
+        _logger.i("Image classified successfully: $_category");
       });
+    } else {
+      _logger.e("Image classification failed: image input is null.");
     }
   }
 
-  List<List<List<double>>> _preprocessImage(img.Image image) {
-    final resizedImage = img.copyResize(image, width: 168, height: 168);
-    return List.generate(
-      224,
+  List<List<List<List<double>>>> _preprocessImage(img.Image image) {
+    final resizedImage = img.copyResize(image, width: 224, height: 224);
+    return [ // Add a batch dimension
+    List.generate(
+      224, // Height
       (y) => List.generate(
-        224,
+        224, // Width
         (x) {
           final pixel = resizedImage.getPixel(x, y);
           return [
-            pixel.r / 255.0, // Extract red value
-            pixel.g / 255.0, // Extract green value
-            pixel.b / 255.0, // Extract blue value
+            pixel.r / 255.0,
+            pixel.g / 255.0,
+            pixel.b / 255.0,
           ];
         },
       ),
-    );
-  }
+    ),
+  ];
+}
 
   Future<String> _getUniqueImageName(String originalName) async {
     String fileName = originalName;
     int count = 1;
 
-    final ref = _storage.ref().child('bytes_format_images/$fileName');
+    final ref = _storage.ref().child('byte_format_images/$fileName');
 
     try {
       await ref.getDownloadURL();
       while (true) {
         fileName =
             '${originalName.split('.').first}_$count.${originalName.split('.').last}';
-        final newRef = _storage.ref().child('bytes_format_images/$fileName');
+        final newRef = _storage.ref().child('byte_format_images/$fileName');
         try {
           await newRef.getDownloadURL();
           count++;
@@ -125,11 +185,12 @@ class AddClothesFieldsState extends State<AddClothesFields> {
       final file = File(imageFile.path);
       try {
         String uniqueFileName = await _getUniqueImageName(imageFile.name);
-        final ref = _storage.ref().child('bytes_format_images/$uniqueFileName');
+        final ref = _storage.ref().child('byte_format_images/$uniqueFileName');
         await ref.putFile(file);
+        _logger.i("Image uploaded successfully: $uniqueFileName");
         return uniqueFileName;
       } catch (e) {
-        print('Error uploading image: $e');
+        _logger.e('Error uploading image: $e');
       }
     }
     return null;
@@ -150,9 +211,10 @@ class AddClothesFieldsState extends State<AddClothesFields> {
           'user_id': widget.userId,
         });
 
+        _logger.i("Clothes data added successfully.");
         Navigator.pop(context);
       } catch (e) {
-        print('Error adding clothes data: $e');
+        _logger.e('Error adding clothes data: $e');
       }
     }
   }
@@ -160,13 +222,13 @@ class AddClothesFieldsState extends State<AddClothesFields> {
   Future<void> _fetchImageData(String imageName) async {
     if (imageName.isNotEmpty) {
       try {
-        final ref = _storage.ref().child('bytes_format_images/$imageName');
+        final ref = _storage.ref().child('byte_format_images/$imageName');
         final imageData = await ref.getData();
         setState(() {
           _imageBytes = imageData;
         });
       } catch (e) {
-        print('Error fetching image data: $e');
+        _logger.e('Error fetching image data: $e');
       }
     }
   }
@@ -243,13 +305,13 @@ class AddClothesFieldsState extends State<AddClothesFields> {
           const SizedBox(height: 12.0),
           _buildTextField(
             label: 'Size',
-            initialValue: _size != null ? _size.toString() : null,
+            initialValue: _size?.toString(),
             onChanged: (value) {
               setState(() {
                 _size = int.tryParse(value);
               });
             },
-            validatorMessage: 'Please enter a valid size',
+            validatorMessage: 'Please enter a size',
             height: fieldHeight,
             keyboardType: TextInputType.number,
           ),
@@ -266,6 +328,11 @@ class AddClothesFieldsState extends State<AddClothesFields> {
             height: fieldHeight,
             keyboardType: TextInputType.number,
           ),
+          const SizedBox(height: 20.0),
+          ElevatedButton(
+            onPressed: submitData,
+            child: const Text('Submit'),
+          ),
         ],
       ),
     );
@@ -273,7 +340,7 @@ class AddClothesFieldsState extends State<AddClothesFields> {
 
   Widget _buildTextField({
     required String label,
-    required String? initialValue,
+    String? initialValue,
     required ValueChanged<String> onChanged,
     required String validatorMessage,
     required double height,
@@ -283,24 +350,24 @@ class AddClothesFieldsState extends State<AddClothesFields> {
       height: height,
       child: TextFormField(
         initialValue: initialValue,
-        decoration: InputDecoration(
-          labelText: label,
-          labelStyle: const TextStyle(color: Colors.black),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: focusedBorderColor, width: 2.0),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: unfocusedBorderColor, width: 1.0),
-          ),
-        ),
+        onChanged: onChanged,
         validator: (value) {
           if (value == null || value.isEmpty) {
             return validatorMessage;
           }
           return null;
         },
-        onChanged: onChanged,
         keyboardType: keyboardType,
+        decoration: InputDecoration(
+          labelText: label,
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: focusedBorderColor),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: unfocusedBorderColor),
+          ),
+          hintText: 'Enter $label',
+        ),
       ),
     );
   }
